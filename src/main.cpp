096f4b8c7d484751d0a26ebae94c1fbe39bb4d58
Nabuki <nabuki@vk.com>
2025-02-07 20:22:50 +0300

    real read

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 
-// NOTE: using internal 12-bit adc - each channel has two bytes in a packet
-#define CHANNELS 16         // Number of channels a packet is carrying
-#define PACKET_RATE_HZ 7000 // Hz
-#define SAMPLE_PIN 4        // Analog pin to read from
+// TODO: maybe oversample the adc to get a more accurate reading
+
+// NOTE: a channel value is read using internal 12-bit adc - each channel has two bytes in a packet
+#define CHANNELS 2          // Number of channels a packet is carrying
+#define PACKET_RATE_HZ 2000 // Hz
+
+const uint8_t pin[CHANNELS] = {3, 11}; // Analog pins to read from
 
 uint16_t packet[CHANNELS + 1];
 const uint8_t *data = reinterpret_cast<const uint8_t *>(&packet);
@@ -11,15 +14,13 @@ const uint8_t *data = reinterpret_cast<const uint8_t *>(&packet);
 uint32_t last_time = 0;
 const uint32_t interval_us = 1000000 / PACKET_RATE_HZ; // Interval in microseconds
 
-uint32_t count = 0;
-
 void setup()
 {
     // USBCDC
     Serial.begin(BAUDRATE);
 
-    // Initialize analog pin
-    // pinMode(SAMPLE_PIN, INPUT);
+    // Initialize analog pins
+    for (size_t i = 0; i < CHANNELS; i++) pinMode(pin[i], INPUT);
 
     // prepare packet end
     packet[CHANNELS] = 0xFFFF;
@@ -32,22 +33,14 @@ void loop()
     // Check if it's time to send the next packet
     if (current_time - last_time >= interval_us)
     {
-        last_time = current_time;
-
-        // Read analog value
-        // uint16_t value = analogRead(SAMPLE_PIN);
-
-        // update count
-        count = (count + 1) % 4096;
-
-        // Populate the packet with dummy data (e.g., incrementing values for testing)
-        for (size_t i = 0; i < CHANNELS; i++)
-        {
-            packet[i] = count;
-        }
+        // Read channels and populate packet
+        for (size_t i = 0; i < CHANNELS; i++) packet[i] = analogRead(pin[i]);
 
         // Send the packet
-        // assert data is not OxFFFF
+        // assert data is not OxFFFF - passes since adc is only 12 bit - the rest of the bits are 0
         Serial.write(data, sizeof(packet));
+
+        // Update time
+        last_time = current_time;
     }
 }
