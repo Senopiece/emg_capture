c88fb77fe9fa0a596d62f7e2a50af84e5d34dfe2
Nabuki <nabuki@vk.com>
2025-02-10 23:30:59 +0300

    use interrupts for timing

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,15 @@ const uint8_t pin[CHANNELS] = {18, 8, 3, 9, 10, 11}; // Analog pins to read from
 uint16_t packet[CHANNELS + 1];
 const uint8_t *data = reinterpret_cast<const uint8_t *>(&packet);
 
-uint32_t last_time = 0;
 const uint32_t interval_us = 1000000 / PACKET_RATE_HZ; // Interval in microseconds
+
+hw_timer_t *timer = NULL;
+volatile bool ready = false;
+
+void IRAM_ATTR onTimer()
+{
+    ready = true;
+}
 
 void setup()
 {
@@ -25,28 +32,20 @@ void setup()
 
     // prepare packet end
     packet[CHANNELS] = 0xFFFF;
+
+    // Setup Timer (ID=0, Divider=80 for 1µs resolution, Mode=autoreload, No ISR)
+    timer = timerBegin(0, 80, true);
+    timerAttachInterrupt(timer, &onTimer, true);
+    timerAlarmWrite(timer, interval_us, true);
+    timerAlarmEnable(timer);
 }
 
 void loop()
 {
-    uint32_t current_time = micros();
-
-    // variance is 35us
-
-    // Check if it's time to send the next packet
-    if (current_time - last_time >= interval_us)
+    if (ready)
     {
-        // Read channels and populate packet
-        for (size_t i = 0; i < CHANNELS; i++) packet[i] = analogRead(pin[i]);
-
-        // Temporary to see the delay variance
-        packet[0] = current_time - last_time;
-
-        // Send the packet
-        // assert data is not OxFFFF - passes since adc is only 12 bit - the rest of the bits are 0
+        for (size_t i = 0; i < CHANNELS; i++) packet[i] = analogRead(pin[i]);        
         Serial.write(data, sizeof(packet));
-
-        // Update time
-        last_time = current_time;
+        ready = false;
     }
 }
