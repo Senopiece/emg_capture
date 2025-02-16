ba72e632c96f447a74f15895c837e632e917294a
Nabuki <nabuki@vk.com>
2025-02-16 04:13:02 +0300

    fixes

--- xiao_adc_sampler/src/main.c
+++ xiao_adc_sampler/src/main.c
@@ -3,25 +3,27 @@
 #include "packet_sink.h"
 #include "sample_channels.h"
 
-// system clock is 32768Hz
-// if taking a sample each 16 ticks
-// we get 32768Hz / 16 = 2048Hz sampling rate
-#define SAMPLE_PERIOD Z_TIMEOUT_TICKS(16)
+#define SAMPLING_FREQUENCY 2048
+#define SAMPLE_PERIOD                                                          \
+  Z_TIMEOUT_TICKS(sys_clock_hw_cycles_per_sec() / SAMPLING_FREQUENCY)
 
 static struct k_timer sample_timer;
+static struct k_work sample_work;
 
-static void sample_timer_handler(struct k_timer *timer) {
-  // this code itself is non-blocking and very fast,
-  // also k_work_submit will introduce 0-30us latency variance
-  // that is not acceptable, so k_work_submit is avoided
+static void sample_work_handler(struct k_work *work) {
   sample_channels();
   send_packet(sampled_channels_buff, CHANNELS);
 }
 
-int main() {
-  init_sample_channels();
-  init_packet_sink();
+static void sample_timer_handler(struct k_timer *timer) {
+  k_work_submit(&sample_work);
+}
 
+int main() {
+  init_packet_sink();
+  init_sample_channels();
+
+  k_work_init(&sample_work, sample_work_handler);
   k_timer_init(&sample_timer, sample_timer_handler, NULL);
   k_timer_start(&sample_timer, SAMPLE_PERIOD, SAMPLE_PERIOD);
 
--- xiao_adc_sampler/src/sample_channels.h
+++ xiao_adc_sampler/src/sample_channels.h
@@ -15,7 +15,7 @@ static const struct device *adc_dev = DEVICE_DT_GET(DT_NODELABEL(adc));
 
 static struct adc_channel_cfg adc_channels[CHANNELS];
 static struct adc_sequence adc_seq;
-static int16_t sampled_channels_buff[CHANNELS];
+static uint16_t sampled_channels_buff[CHANNELS];
 
 void init_sample_channels(void) {
   for (int i = 0; i < CHANNELS; i++) {
@@ -23,14 +23,15 @@ void init_sample_channels(void) {
     adc_channels[i].differential = 0;
 
     /* Use maximum acquisition time to reduce noise */
-    adc_channels[i].acquisition_time = ADC_ACQ_TIME_MAX;
+    adc_channels[i].acquisition_time = ADC_ACQ_TIME_DEFAULT;
     adc_channels[i].reference = ADC_REF_INTERNAL; // 0.6 V internal reference
 
     /* Use gain of 1/6 to allow 3.6V input (3.6V * 1/6 ≈ 0.6V) */
     adc_channels[i].gain = ADC_GAIN_1_6;
 
 #ifdef CONFIG_ADC_CONFIGURABLE_INPUTS
-    /* Map each channel to an ADC input pin. For nRF, assume channels 0-5 map to
+    /* Map each channel to an ADC input pin. For nRF, assume channels 0-5 map
+    to
      * AIN0-AIN5 */
     adc_channels[i].input_positive = NRF_SAADC_INPUT_AIN0 + i;
 #endif
